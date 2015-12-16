c9390564daaf90ef73ea53288336c59bc6dd1946
sunhick@gmail.com <Sunhick@gmail.com>
2015-12-16 12:11:35 -0700

    solution for security functions

---
+++ Security/Functions/Security-Functions.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+__author__ = "Sunil"
+__copyright__ = "Copyright 2015, hacker_rank Project"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__email__ = "sunhick@gmail.com"
+
+# Complete the function below.
+
+
+def function(x):
+    return x % 11
