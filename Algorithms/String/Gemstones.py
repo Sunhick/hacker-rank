888ad306d573638ee06c4dfcd4289d4a27041acc
Sunil <sunhick@gmail.com>
2016-05-24 19:37:02 -0600

    removed unwanted code

--- Algorithms/String/Gemstones.py
+++ Algorithms/String/Gemstones.py
@@ -9,13 +9,6 @@ __license__ = "MIT"
 __version__ = "1.0.0"
 __email__ = "sunhick@gmail.com"
 
-def count_gems(a, b):
-	ce = ''
-	for c in a:
-		if c in b:
-			ce = ce + c
-	return ce
-
 n = int(input().split()[0])
 rocks = [input() for i in range(n)]
 counters = [Counter(rock) for rock in rocks]
